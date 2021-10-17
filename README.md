d1740aed54178e4c68ccd3173f156fbcae42c2da
Roberto Serra <roberto.serra@etiqa.it>
2021-10-18 00:19:11 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Shortcut Keyboard
+
+Small scriptable keyboard created with a 3d Printer, an Arduino Micro Pro and a tiny bit of code
+
+
+## Images:
+Switches back:
+![Back switches](img/05ec7e17-52ee-4c81-9110-7d9830df107a.jpeg)
+Arduino Board:
+![Arduino board](img/543acd01-9b67-47a0-a4fb-dcb498af2472.jpeg)
+Circuit:
+![Circuit](img/917a09f2-c82c-4e07-b287-2b5e941ea6c6.jpeg)
+Circuit 2:
+![Circuit 2](img/82811277-181c-4c20-a44c-e7d2f5572a9f.jpeg)
+Keyboard:
+![keyboard](img/5725ab47-4cff-4ced-bc0d-4bef3673fb6e.jpeg)
+
+## 3d Resources files
+
+Stream Deck/ Marco Keyboard [Stream-deck](https://www.thingiverse.com/thing:4186055)
+The keycaps I ended up using [Keycap](https://www.thingiverse.com/thing:738769)
---
+++ shortcut_keyboard.ino
@@ -0,0 +1,204 @@
+#include <Keyboard.h>
+
+using namespace std;
+
+typedef struct
+{
+  int pin;
+  String keyName;
+  uint8_t lastRead;
+} button;
+
+button buttons[12] = {
+    {4, "Button1", HIGH},
+    {3, "Button2", HIGH},
+    {2, "Button3", HIGH},
+    {15, "Button4", HIGH},
+    {5, "Button5", HIGH},
+    {6, "Button6", HIGH},
+    {7, "Button7", HIGH},
+    {8, "Button8", HIGH},
+    {9, "Button9", HIGH},
+    {10, "Button10", HIGH},
+    {16, "Button11", HIGH},
+    {14, "Button12", HIGH},
+};
+
+uint8_t lastReads[12] = {};
+
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(50);
+  Keyboard.release(key);
+}
+
+void openApp(String appName)
+{
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(' ');
+  Keyboard.releaseAll();
+
+  delay(400);
+
+  Keyboard.print(appName);
+
+  delay(300);
+
+  typeKey(KEY_RETURN);
+
+  delay(300);
+}
+
+//https://www.arduino.cc/en/Reference/KeyboardModifiers
+
+void dittoStackOpen()
+{
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press('v');
+  Keyboard.releaseAll();
+}
+
+void vsCodeBeauty()
+{
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press('f');
+  Keyboard.releaseAll();
+}
+
+void vsCodeSave()
+{
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('s');
+  Keyboard.releaseAll();
+}
+
+void leftDesktop()
+{
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_ARROW);
+  Keyboard.releaseAll();
+}
+
+void rightDesktop()
+{
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_RIGHT_ARROW);
+  Keyboard.releaseAll();
+}
+
+void hideApplication()
+{
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('m');
+  Keyboard.releaseAll();
+}
+
+void spectacleFullScreen()
+{
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('f');
+  Keyboard.releaseAll();
+}
+
+void spectacleCenterScreen()
+{
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press('c');
+  Keyboard.releaseAll();
+}
+
+void spectacleLeftScreen()
+{
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_LEFT_ARROW);
+
+  Keyboard.releaseAll();
+}
+
+void spectacleRightScreen()
+{
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(KEY_LEFT_ALT);
+  Keyboard.press(KEY_RIGHT_ARROW);
+
+  Keyboard.releaseAll();
+}
+
+void setup()
+{
+
+  Serial.begin(9600); // open the serial port at 9600 bps:
+  Keyboard.begin();
+  for (int i = 0; i < 12; i++)
+  {
+    button btn = buttons[i];
+    int pin = btn.pin;
+    Serial.println("Binding" + btn.keyName);
+    pinMode(pin, INPUT_PULLUP);
+    lastReads[i] = digitalRead(pin);
+  }
+}
+
+void loop()
+{
+
+  for (int i = 0; i < 12; i++)
+  {
+    button btn = buttons[i];
+    int pin = btn.pin;
+    uint8_t currentRead = digitalRead(pin);
+    if (currentRead == LOW && lastReads[i] == HIGH)
+    {
+
+      if (btn.keyName == "Button1")
+      {
+        vsCodeBeauty();
+      }
+      else if (btn.keyName == "Button2")
+      {
+        vsCodeSave();
+      }
+      else if (btn.keyName == "Button3")
+      {
+        dittoStackOpen();
+      }
+      else if (btn.keyName == "Button6")
+      {
+        spectacleFullScreen();
+      }
+      else if (btn.keyName == "Button5")
+      {
+        spectacleLeftScreen();
+      }
+      else if (btn.keyName == "Button8")
+      {
+        spectacleRightScreen();
+      }
+      else if (btn.keyName == "Button7")
+      {
+        //hideApplication();
+        spectacleCenterScreen();
+      }
+      else if (btn.keyName == "Button9")
+      {
+        leftDesktop();
+      }
+      else if (btn.keyName == "Button12")
+      {
+        rightDesktop();
+      }
+
+      Serial.println("PRESSED " + btn.keyName);
+    }
+
+    lastReads[i] = currentRead;
+  }
+}
